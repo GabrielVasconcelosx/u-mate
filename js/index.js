f9f08b4eca788e1d3ff68c3ea863938e707f9f7b
Gabriel Moreno <Home@MacBook-Air.local>
2019-05-16 01:20:03 -0300

    Tela de Compartilhar

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#tab2').hide();
+    $('#tab3').hide();
+    var status = $('#status');
+    $('#btn-singup').click(function(){
+        var progress = $('#progress');
+        if(status.val() == 1) {
+            $('#tab1').hide();
+            $('#tab2').show();
+            progress.text('Compartilhar');
+            progress.css('width','66%')
+            status.val(2)
+        }
+        else if(status.val() == 2) {
+            $('#tab2').hide();
+            $('#tab3').show();
+            progress.text('Esperar');
+            progress.css('width','100%')
+        }
+    });
+
+});
