5a873da7a9be5cdd03acb37bd0a22a36d664bfb2
Gabriel Moreno <Home@MacBook-Air.local>
2019-05-31 10:20:52 -0300

    WIP

--- js/index.js
+++ js/index.js
@@ -21,20 +21,31 @@ $(document).ready(function(){
     });
     
     $('.btn-outline-success ').on('click', function(){
-        $(this).html('<i class="fas fa-check"></i> Pedido Enviado').attr("disabled", true);;
+        $(this).html('<i class="fas fa-check"></i> Pedido Enviado').attr("disabled", true);
         toastr.success('Pedido enviado com sucesso')
+
+    });
+    $('#btnAceitar').on('click', function(){
+        $(this).html('<i class="fas fa-check"></i> Pedido aceito').attr("disabled", true);
+        toastr.success('Pedido aceito!')
+
+    });
+    $('#btnRecusar').on('click', function(){
+        $(this).html('<i class="fas fa-check"></i> Pedido recusado').attr("disabled", true);
+        toastr.error('Pedido recusado!')
 
     });
 
     $('#btnLogin').click(function(){
-        if ($('#login').val() != 'jose@mail.com') {
+        if ($('#login').val() != 'jose@mail.com' || $('#senha').val() != 'senha') {
             $('#alert').removeClass('d-none');
         }
         else {
+            $(this).html('<i class="fas fa-spinner fa-pulse"></i></span> Logando').attr("disabled", true);
             setTimeout(function(){
                     window.location.href = "http://u-mate.test/pedidos.html";
                     toastr.success('Logado com Sucesso');
-            }, 10000);
+            }, 3000);
 
         }
     });
